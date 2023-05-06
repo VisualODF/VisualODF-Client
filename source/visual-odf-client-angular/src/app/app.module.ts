8d7e6ebc52e98a91b063f24a294d8d3c4987ef25
ROG15\bladexue <blade1565@outlook.com>
2023-05-07 01:16:19 +0800

    try to import iconfont:
    1. import iconfont
    2. try to finish the basic layout

--- source/visual-odf-client-angular/src/app/app.module.ts
+++ source/visual-odf-client-angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopologyComponent } from './topology/topology.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopologyComponent
+    TopologyComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
